79386a77a6925809ab50191105d7d9cd46f6da49
shainaloria <shainaloria@gmail.com>
2019-12-27 09:18:51 +0800

    Updated babel configuration

---
+++ babel.config.js
@@ -0,0 +1,24 @@
+const path = require('path')
+
+module.exports = function(api) {
+  api.cache(true);
+  return {
+    presets: ['babel-preset-expo'],
+    plugins: [
+      [
+        'module-resolver',
+        {
+          alias: {
+            assets: path.resolve(__dirname, 'assets'),
+            src: path.resolve(__dirname, 'src'),
+          },
+        },
+      ],
+    ],
+    env: {
+      development: {
+        'plugins': ['@babel/plugin-transform-react-jsx-source'],
+      },
+    },
+  }
+}
